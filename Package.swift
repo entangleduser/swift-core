a4525f6b8f9982d421761c6ec30592e58f26206d
Will <70107970+neutralradiance@users.noreply.github.com>
2022-05-20 07:42:39 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,63 +1,77 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.6
 import PackageDescription
 
 let package = Package(
-	name: "Core",
-	platforms: [.macOS(.v10_15), .iOS(.v13)],
-	products: [
-		.library(
-			name: "Core",
-			targets: [
-				"Core",
-				"Colors",
-				"Storage",
-				"Structures",
-				"Swizzle",
-				"UI"
-			]
-		)
-	],
-	dependencies: [
-		.package(url: "https://github.com/apple/swift-algorithms", from: "0.2.1")
-	],
-	targets: [
-		.target(
-			name: "Core",
-			dependencies: [
-				.product(name: "Algorithms", package: "swift-algorithms")
-			]
-		),
-		.target(
-			name: "Colors",
-			dependencies: ["Core"]
-		),
-		.target(
-			name: "Storage",
-			dependencies: ["Core"]
-		),
-		.target(
-			name: "Structures",
-			dependencies: ["Core"]
-		),
-		.target(
-			name: "Swizzle",
-			dependencies: ["Core"]
-		),
-		.target(
-			name: "UI",
-			dependencies: ["Core"]
-		),
-		.testTarget(
-			name: "CoreTests",
-			dependencies: ["Core"]
-		),
-		.testTarget(
-			name: "StorageTests",
-			dependencies: ["Storage"]
-		),
-		.testTarget(
-			name: "StructuresTests",
-			dependencies: ["Structures"]
-		)
-	]
+ name: "Core",
+ platforms: [.macOS(.v11), .iOS(.v15)],
+ products: [
+  .library(name: "Core", targets: ["Core"]),
+  .library(name: "Colors", targets: ["Colors"]),
+  .library(name: "Storage", targets: ["Storage"]),
+  .library(name: "Structures", targets: ["Structures"]),
+  .library(name: "Swizzle", targets: ["Swizzle"]),
+  .library(name: "Reflection", targets: ["Reflection"]),
+  .library(name: "Views", targets: ["Views"])
+ ],
+ dependencies: [
+  .package(url: "https://github.com/apple/swift-collections", branch: "main"),
+  .package(url: "https://github.com/apple/swift-algorithms", branch: "main"),
+  .package(url: "https://github.com/siteline/SwiftUI-Introspect", branch: "master")
+ ],
+ targets: [
+  .target(
+   name: "Core",
+   dependencies: [
+    .product(name: "Algorithms", package: "swift-algorithms")
+   ]
+  ),
+  .target(
+   name: "Colors",
+   dependencies: ["Core"]
+  ),
+  .target(
+   name: "Storage",
+   dependencies: ["Core", "Reflection"]
+  ),
+  .target(
+   name: "Structures",
+   dependencies: [
+    "Core",
+    .product(name: "Collections", package: "swift-collections")
+   ]
+  ),
+  .target(
+   name: "Swizzle",
+   dependencies: ["Core"]
+  ),
+  .target(
+   name: "Reflection",
+   dependencies: ["Core"]
+  ),
+  .target(
+   name: "Views",
+   dependencies: [
+    "Core",
+    "Structures",
+    "Colors",
+    "Storage",
+    .product(name: "Introspect", package: "swiftui-introspect")
+   ],
+   resources: [
+    .process("Resources")
+   ]
+  ),
+  .testTarget(
+   name: "CoreTests",
+   dependencies: ["Core"]
+  ),
+  .testTarget(
+   name: "StorageTests",
+   dependencies: ["Storage"]
+  ),
+  .testTarget(
+   name: "StructuresTests",
+   dependencies: ["Structures"]
+  )
+ ]
 )
